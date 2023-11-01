5a9541dd598dcfe277bc98abfda5773510a80a4b
Krisrina Chem <140569870+KrisrinaChem@users.noreply.github.com>
2023-11-01 09:04:19 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,17 @@
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+    const data = [];
+}
+    data.forEach(item => {
+    });
+    for (let i = 0; i < 10; i++) {
+    return data;
+}
+
+    const data = generateRandomData();
+
+        console.log("Random Number: " + item);
+
+    }
+main();
