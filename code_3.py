4732bf3657a0647814cffa0a43a8b5d989144d5a
Krisrina Chem <140569870+KrisrinaChem@users.noreply.github.com>
2023-11-09 17:55:16 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+def main():
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+
+    main()
+    return data
+    for item in data:
+import random
