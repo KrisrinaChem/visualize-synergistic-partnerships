e5ead7267397929293261ed5825093c3c53a5476
Krisrina Chem <140569870+KrisrinaChem@users.noreply.github.com>
2023-11-12 12:57:47 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,66 +1,27 @@
-    for item in data:
+def main():
+
+
         print(f"Random Number: {item}")
-    data = generate_random_data()
-
     main()
-def generate_random_data():
-    return data
-
-
-if __name__ == "__main__":
     data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()
+    return data
+    for item in data:
+
+def generate_random_data():
+if __name__ == "__main__":
 import random
 
-def main():        }
-
-
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-            data[i] = (int) (Math.random() * 100) + 1;
-    }
-}
-    public static void main(String[] args) {
-        for (int item : data) {
-        int[] data = new int[10];
-        }
-        for (int i = 0; i < 10; i++) {            System.out.println("Random Number: " + item);
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-    }
-        int[] data = new int[10];
-        for (int i = 0; i < 10; i++) {
-
-    public static void main(String[] args) {
-
-        for (int item : data) {
-public class RandomDataGenerator {
-}
-        }    for (let i = 0; i < 10; i++) {
-main();
-    return data;
-    }
-        console.log("Random Number: " + item);
-function generateRandomData() {
-    const data = generateRandomData();
-}
-}
-function main() {
-    });
-
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-
-    data.forEach(item => {
-    const data = [];    for item in data:
-if __name__ == "__main__":
-
-def generate_random_data():
-
-
-        print(f"Random Number: {item}")
     data = generate_random_data()
+
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
     main()
 def main():
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+if __name__ == "__main__":
+    for item in data:
     return data
